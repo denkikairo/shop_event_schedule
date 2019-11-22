c333218a5ba9c6e5c291bfdf86fed24b6757e301
yusuke <denkikairo@gmail.com>
2019-11-22 16:16:04 +0900

    Feature: deviseによるログイン機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :shops, :controllers => {
+    :registrations => 'shops/registrations',
+    :sessions => 'shops/sessions'
+  }
+
+	root to: 'shops#index'
+	resources :shops, only: %i[index]
 end
