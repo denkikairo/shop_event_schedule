e6ee67900a39068572edbd7285965f846d31d95c
yusuke <denkikairo@gmail.com>
2020-01-24 18:23:18 +0900

    Fix: 不要なコメントのため削除

--- app/assets/javascripts/calendar.js
+++ app/assets/javascripts/calendar.js
@@ -96,33 +96,5 @@ $('#CalendarBookmarks').fullCalendar({
         $eventElem = $element.find('span.fc-title');
         $eventElem.html(titleStr); // htmlとして出力
     }
-})
-// $('#CalendarShop').fullCalendar({
-//     events: '/shops/events.json',
-//     timeFormat: 'H:mm',
-//     eventColor: '#63ceef',
-//     lang: 'ja',
-//     eventClick: function(calEvent, jsEvent, view) {
-//       //イベント編集のためeditアクションを発火
-//       $.ajax({
-//         type: 'GET',
-//         url: '/events/' + calEvent.id
-//       }).done(function (res) {
-//         //イベント編集用のhtmlを作成
-//         $('.modal-body').html(res);
-//         $('#modal').modal();
-//       }).fail(function (result) {
-//         // 失敗処理
-//         alert('エラーが発生しました。運営に問い合わせてください。')
-//       });
-//     },
-//     eventRender: function(event, $element) {
-//       console.log("aaaa")
-//         var titleStr = $element.find('span.fc-title').text(), // htmlタグを含むtitleの文字列を取得
-//         // console.log(titleStr)
-//         $eventElem = $element.find('span.fc-title');
-//
-//         $eventElem.html(titleStr); // htmlとして出力
-//     }
-// })
+  })
 });
