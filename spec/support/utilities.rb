663b81a744195226a71fb729e827bdc6ec9d0de6
yusuke <denkikairo@gmail.com>
2020-01-30 17:29:41 +0900

    Fix: device日本語化、shop loginリダイレクト変更に対応していなかったため

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -4,5 +4,5 @@ def login_as(shop)
   visit new_shop_session_path
   fill_in 'shop_email', with: shop.email
   fill_in 'shop_password', with: 'password'
-  click_button 'Log in'
+  click_button 'ログイン'
 end
